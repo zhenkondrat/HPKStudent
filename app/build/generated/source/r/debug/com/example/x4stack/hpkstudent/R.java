deeee0fa798c85dce0341ef333299b97028f68c4
AndreyKogut <andreyko.ol@gmail.com>
2015-04-16 12:36:57 +0300

    Addition

--- app/build/generated/source/r/debug/com/example/x4stack/hpkstudent/R.java
+++ app/build/generated/source/r/debug/com/example/x4stack/hpkstudent/R.java
@@ -1435,22 +1435,22 @@ containing a value of this type.
         public static final int action_bar_subtitle=0x7f09001f;
         public static final int action_bar_title=0x7f09001e;
         public static final int action_context_bar=0x7f090032;
-        public static final int action_example=0x7f09004f;
+        public static final int action_example=0x7f090050;
         public static final int action_menu_divider=0x7f090002;
         public static final int action_menu_presenter=0x7f090003;
         public static final int action_mode_bar=0x7f09002e;
         public static final int action_mode_bar_stub=0x7f09002d;
         public static final int action_mode_close_button=0x7f090020;
-        public static final int action_settings=0x7f09004e;
+        public static final int action_settings=0x7f09004f;
         public static final int activity_chooser_view_content=0x7f090021;
-        public static final int album_cover=0x7f090042;
-        public static final int album_description=0x7f090044;
-        public static final int album_list=0x7f090045;
-        public static final int album_name=0x7f090043;
+        public static final int album_cover=0x7f090044;
+        public static final int album_description=0x7f090046;
+        public static final int album_list=0x7f090042;
+        public static final int album_name=0x7f090045;
         public static final int always=0x7f090016;
         public static final int beginning=0x7f090013;
         public static final int checkbox=0x7f090029;
-        public static final int classRoom=0x7f09004d;
+        public static final int classRoom=0x7f09004e;
         public static final int collapseActionView=0x7f090017;
         public static final int container=0x7f090040;
         public static final int decor_content_parent=0x7f09002f;
@@ -1463,13 +1463,13 @@ containing a value of this type.
         public static final int end=0x7f090014;
         public static final int expand_activities_button=0x7f090022;
         public static final int expanded_menu=0x7f090028;
-        public static final int group=0x7f090048;
+        public static final int group=0x7f090049;
         public static final int home=0x7f090004;
         public static final int homeAsUp=0x7f09000d;
         public static final int icon=0x7f090026;
         public static final int ifRoom=0x7f090018;
         public static final int image=0x7f090023;
-        public static final int lesson=0x7f09004b;
+        public static final int lesson=0x7f09004c;
         public static final int listMode=0x7f090009;
         public static final int list_item=0x7f090025;
         public static final int middle=0x7f090015;
@@ -1477,11 +1477,11 @@ containing a value of this type.
         public static final int never=0x7f090019;
         public static final int none=0x7f09000e;
         public static final int normal=0x7f09000a;
-        public static final int numbOfLesson=0x7f090049;
+        public static final int numbOfLesson=0x7f09004a;
         public static final int progress_circular=0x7f090005;
         public static final int progress_horizontal=0x7f090006;
         public static final int radio=0x7f09002b;
-        public static final int replacement_list=0x7f090047;
+        public static final int replacement_list=0x7f090048;
         public static final int search_badge=0x7f090035;
         public static final int search_bar=0x7f090034;
         public static final int search_button=0x7f090036;
@@ -1492,7 +1492,7 @@ containing a value of this type.
         public static final int search_plate=0x7f090039;
         public static final int search_src_text=0x7f09003a;
         public static final int search_voice_btn=0x7f09003e;
-        public static final int section_label=0x7f090046;
+        public static final int section_label=0x7f090047;
         public static final int shortcut=0x7f09002a;
         public static final int showCustom=0x7f09000f;
         public static final int showHome=0x7f090010;
@@ -1500,11 +1500,12 @@ containing a value of this type.
         public static final int split_action_bar=0x7f090007;
         public static final int submit_area=0x7f09003c;
         public static final int tabMode=0x7f09000b;
-        public static final int teacher=0x7f09004c;
+        public static final int teacher=0x7f09004d;
         public static final int title=0x7f090027;
         public static final int up=0x7f090008;
         public static final int useLogo=0x7f090012;
-        public static final int whosRaplacement=0x7f09004a;
+        public static final int view_pager=0x7f090043;
+        public static final int whosRaplacement=0x7f09004b;
         public static final int withText=0x7f09001a;
         public static final int wrap_content=0x7f09001d;
     }
@@ -1537,17 +1538,21 @@ containing a value of this type.
         public static final int abc_search_view=0x7f040014;
         public static final int abc_simple_dropdown_hint=0x7f040015;
         public static final int activity_main=0x7f040016;
-        public static final int album_view=0x7f040017;
-        public static final int fragment_album=0x7f040018;
-        public static final int fragment_main=0x7f040019;
-        public static final int fragment_navigation_drawer=0x7f04001a;
-        public static final int fragment_replacement=0x7f04001b;
-        public static final int replacement_view=0x7f04001c;
-        public static final int support_simple_spinner_dropdown_item=0x7f04001d;
+        public static final int activity_news_fragment=0x7f040017;
+        public static final int activity_photo=0x7f040018;
+        public static final int album_view=0x7f040019;
+        public static final int fragment_album=0x7f04001a;
+        public static final int fragment_main=0x7f04001b;
+        public static final int fragment_navigation_drawer=0x7f04001c;
+        public static final int fragment_replacement=0x7f04001d;
+        public static final int replacement_view=0x7f04001e;
+        public static final int support_simple_spinner_dropdown_item=0x7f04001f;
     }
     public static final class menu {
         public static final int global=0x7f0d0000;
         public static final int main=0x7f0d0001;
+        public static final int menu_news=0x7f0d0002;
+        public static final int menu_photo=0x7f0d0003;
     }
     public static final class mipmap {
         public static final int ic_launcher=0x7f030000;
